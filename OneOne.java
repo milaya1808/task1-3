555baa88686b9d3d5a03d0ae73d295e78facfb76
milaya1808 <83075592+milaya1808@users.noreply.github.com>
2021-05-05 20:45:15 +0300

    Add files via upload

---
+++ OneOne.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+    String firstname = "Elizaveta";
+    String lastname = "Kuzina";
+    int age = 24;
+    String city = "Moscow";
+    System.out.println("Hello "+firstname+" "+lastname+" - "+age+" age"+" Moscow");
+	// write your code here
+    }
+}
---
+++ OneTwo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 6;
+        System.out.println(" 3 ");
+        System.out.println("плюс");
+        System.out.println(" 6 ");
+        System.out.println("равно");
+        System.out.println( a + b );
+	// write your code here
+    }
+}
